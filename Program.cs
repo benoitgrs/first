6bb148ffb8a10499191646e9ea83e4d85f4785e4
Benoit <24571@student.dorset-college.ie>
2021-09-29 17:18:48 +0100

    message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nouveau_poo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
